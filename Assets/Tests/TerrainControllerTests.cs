2768a6a9db919c2f33a4f783a5bbd76318a3e54e
Ke Ren <renke0907@gmail.com>
2023-11-24 10:12:04 +0100

    Update TerrainControllerTests.cs

--- Assets/Tests/TerrainControllerTests.cs
+++ Assets/Tests/TerrainControllerTests.cs
@@ -11,6 +11,7 @@ public class TerrainControllerTests
     [SetUp]
     public void SetUp()
     {
+        // Initialize a new GameObject and add the TerrainController component to it for testing.
         _terrainGameObject = new GameObject("Terrain");
         _terrainController = _terrainGameObject.AddComponent<TerrainController>();
     }
@@ -18,35 +19,35 @@ public class TerrainControllerTests
     [TearDown]
     public void TearDown()
     {
-        // Cleanup
+        // Clean up the GameObject created during the test to prevent memory leaks or test interference.
         Object.DestroyImmediate(_terrainGameObject);
     }
 
     [Test]
     public void Speed_SetAndGet_ReturnsCorrectValue()
     {
-        // Arrange
+        // Define the expected speed value for testing.
         float expectedSpeed = 5f;
 
-        // Act
+        // Set the speed on the TerrainController and then retrieve it.
         _terrainController.setSpeed(expectedSpeed);
         float actualSpeed = _terrainController.getSpeed();
 
-        // Assert
+        // Verify that the set speed is correctly retrieved, ensuring the getter and setter work as expected.
         Assert.AreEqual(expectedSpeed, actualSpeed);
     }
 
     [Test]
     public void TerrainPosition_ResetsCorrectly_WhenMovedTooFar()
     {
-        // Arrange
+        // Set a high speed for the test and move the terrain beyond the reset limit.
         _terrainController.setSpeed(100f); // Set a high speed for the test
         _terrainGameObject.transform.position = new Vector3(0, 0, -1001); // Set the terrain beyond the reset limit
 
-        // Act
+        // Trigger the Update method, which should reset the terrain's position.
         _terrainController.Update();
 
-        // Assert
+        // Verify that the terrain's position is reset correctly when it moves too far in the negative Z-axis direction.
         Assert.AreEqual(new Vector3(0, 0, 1000), _terrainGameObject.transform.position);
     }
 
